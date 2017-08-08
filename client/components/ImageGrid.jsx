43c0ede24cf5c869f8114fcf83cdaca598acb2b4
sfo <sfragua83@gmail.com>
2017-08-08 21:23:47 +0200

    imagegrid experience

--- client/components/ImageGrid.jsx
+++ client/components/ImageGrid.jsx
@@ -5,7 +5,7 @@ import { Row, Col } from 'antd';
 const ImageGrid = () => (
     <div className="gutter-example">
     <Row className="grid-margins" gutter={16}>
-      <Col xs={24} sm={24} md={8} lg={8} xl={8}><img alt="example" width="100%" src="https://res.cloudinary.com/dri9jzjlr/image/upload/v1499018451/jpn_01.jpg" /></Col>
+      <Col xs={24} sm={24} md={8} lg={8} xl={8}><div className="photo-text"><h1>JAPAN</h1></div><img alt="example" width="100%" src="https://res.cloudinary.com/dri9jzjlr/image/upload/v1499018451/jpn_01.jpg" /></Col>
       <Col xs={24} sm={12} md={8} lg={8} xl={8}><img alt="example" width="100%" src="https://res.cloudinary.com/dri9jzjlr/image/upload/v1499018451/jpn_01.jpg" /></Col>
       <Col xs={24} sm={12} md={8} lg={8} xl={8}><img alt="example" width="100%" src="https://res.cloudinary.com/dri9jzjlr/image/upload/v1499018451/jpn_01.jpg" /></Col>
     </Row>
