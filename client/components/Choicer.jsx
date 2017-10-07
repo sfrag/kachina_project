b69880ecfd84ed4479f9ff23ec53660fb53f3a71
Álvaro Cabanas <acabanas@bodas.net>
2017-10-07 17:31:08 +0200

    Temporary Back Button

--- client/components/Choicer.jsx
+++ client/components/Choicer.jsx
@@ -7,13 +7,15 @@
 import React from 'react'
 
 import MenuComponent from './Menu.jsx'
+import BackButton from './BackButton.jsx'
 import Countries from './Countries.jsx'
 import CountryDetail from './CountryDetail.jsx'
 
 const ChoicerPage = (props) => (
     <div className="root-wrapper">
-        <MenuComponent></MenuComponent>
         <div className="content-wrapper">
+            <MenuComponent></MenuComponent>
+            <BackButton>{props}</BackButton>
             {!props.match.params.country?
                 ( <Countries></Countries> ) :
                 ( <CountryDetail>{props}</CountryDetail>) }
