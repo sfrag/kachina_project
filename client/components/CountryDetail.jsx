1327520784d0367b5627aee07ce5fc90d0f769d2
Álvaro Cabanas <acabanas@bodas.net>
2017-10-10 23:42:54 +0200

    Detail

--- client/components/CountryDetail.jsx
+++ client/components/CountryDetail.jsx
@@ -3,11 +3,15 @@ import { Link } from 'react-router-dom'
 //import { Grid, Image, Container } from 'semantic-ui-react'
 import { Row, Col } from 'antd';
 
+var objects = ['Tokyo', 'Kyoto', 'Miyajima', 'Hiroshima'];
+
 const CountryDetail = (props) => (
     <div className="gutter-example">
-        <Row className="grid-margins" gutter={16}>
-            <Col xs={24} sm={24} md={8} lg={8} xl={8}><div className="photo-text"><h1>{props.children.match.params.country}</h1></div><img alt="example" width="100%" src="https://res.cloudinary.com/dri9jzjlr/image/upload/v1499018451/jpn_01.jpg" /></Col>
-        </Row>
+
+        {objects.map(function(object, i){
+            return <Col xs={24} sm={24} md={8} lg={8} xl={8} key={i}><div className="photo-text"><h1>{object}</h1></div><img alt="example" width="100%" src="https://res.cloudinary.com/dri9jzjlr/image/upload/v1499018451/jpn_01.jpg" /></Col>;
+        })}
+
   </div>);
 
 
